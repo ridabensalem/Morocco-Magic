f132aad0cd92b73f4644f456622c4ccd12f6b130
ridabensalem <ridasbnesalmas@gmail.com>
2023-01-12 18:53:11 +0100

    Loaded speakers section dinamically

--- speakersload.js
+++ speakersload.js
@@ -1,115 +1,79 @@
-const desktopSpeakers = document.querySelector('.container.down_section');
-const divisionSpeaker = ` <div class="row">
-<div class="col-12">
-    <center>
-        <h2>Featured speakers</h2>
-    </center>
-</div>
-</div>
+const speakersData = [
+  {
+    name: 'Steve Yamato',
+    proffesionalism: 'Full stack analysis of google',
+    img: 'images/speaker_01.png',
+    description: 'Focusing on a collaborative approach in a networked environment, he created the concept of co-production based on sharing, such as open source software and Wikipedia.',
+  },
+  {
+    name: 'Peter Meraid',
+    proffesionalism: 'Tourism agent by AFG',
+    img: 'images/speaker_02.png',
+    description: 'Trying to make poeple who visiti Morocco satisfied',
+  },
+  {
+    name: 'Lila pulse',
+    proffesionalism: 'Professor at Rabat Mo',
+    img: 'images/speaker_03.png',
+    description: 'Focusing on a collaborative approach in a networked environment, he created the concept of co-production based on sharing, such as open source software and Wikipedia.',
+  },
+  {
+    name: 'Emma roden',
+    proffesionalism: 'Senior software developer',
+    img: 'images/speaker_04.png',
+    description: 'Testing the platform of eccommerce Moroocco',
+  },
+  {
+    name: 'Amy gorden',
+    proffesionalism: 'Professor at Tanger Law School',
+    img: 'images/speaker_05.png',
+    description: 'Providing the nessecary documents about rules of Tourisme in Morocco.',
+  },
+  {
+    name: 'Ahmed Magma',
+    proffesionalism: 'General director',
+    img: 'images/speaker_06.png',
+    description: 'Organisating the National youth Institute of tourists in Morocco.',
+  },
+];
+function populateSpeakersCards(speakers) {
+  const container = document.querySelector('#desktop_speakers');
+  const speakerCards = container.querySelectorAll('.col-6');
 
-<div class="row speakers_cards">
-<div class="col-6">
-    <div class="row">
-        <div class="col-4 speakers">
-            <img src="images/speaker_01.png" alt="">
-        </div>
-        <div class="col-8">
-            <p>
-                <span class="name">Steven benkler</span>
-                <br>
-                <span class="proffesionalism">
-                    professor at Nessan Law School
-                </span>
-                <br> Practicing  on a collaborative approach in a networked environment, he created the concept
-                of co-production based on sharing, such as open source software and Wikipedia.</p>
-        </div>
-    </div>
-</div>
-<div class="col-6">
-    <div class="row speakers">
-        <div class="col-4">
-            <img src="images/speaker_02.png" alt="">
-        </div>
-        <div class="col-8">
-            <p>
-                <span class="name">Ahmed Magma</span>
-                <br>
-                <span class="proffesionalism">professor at Harvard Law School</span>
-                <br> Focusing on a collaborative approach in a networked environment, he created the concept
-                of co-production based on sharing, such as open source software and Wikipedia.</p>
-        </div>
-    </div>
-</div>
-</div>
+  for (let i = 0; i < speakers.length; i += 1) {
+    const speakerData = speakers[i];
+    const speakerCard = speakerCards[i];
+    const img = speakerCard.querySelector('img');
+    img.src = speakerData.img;
+    img.alt = speakerData.name;
+    const content = speakerCard.querySelector('p');
+    content.innerHTML = `
+        <span class="name">${speakerData.name}</span>
+        <br>
+        <span class="proffesionalism">${speakerData.proffesionalism}</span>
+        <br> ${speakerData.description}
+      `;
+  }
+}
 
-<div class="row speakers_cards">
-<div class="col-6">
-    <div class="row">
-        <div class="col-4 speakers">
-            <img src="images/speaker_03.png" alt="">
-        </div>
-        <div class="col-8">
-            <p>
-                <span class="name">Jess Ghith</span>
-                <br>
-                <span class="proffesionalism">
-                    professor at Harvard Law School
-                </span>
-                <br> Focusing on a collaborative approach to Moroccan culture, this October's festival promotes the idea of co-creation based on sharing and openness.</p>
-        </div>
-    </div>
-</div>
-<div class="col-6">
-    <div class="row speakers">
-        <div class="col-4">
-            <img src="images/speaker_04.png" alt="">
-        </div>
-        <div class="col-8">
-            <p>
-                <span class="name">Rebecca benkler</span>
-                <br>
-                <span class="proffesionalism">
-                Tester at Safi Art School
-            </span>
-            <p>Making partnership about new tourisme in north Morocco to the south then testing traverls experiense </p>
-        </div>
-    </div>
-</div>
-</div>
+populateSpeakersCards(speakersData);
 
+function Mobile(speakers) {
+  const container = document.querySelector('#mobile_skpeakers');
+  const speakerCards = container.querySelectorAll('.mobile-load');
+  for (let i = 0; i < speakers.length; i += 1) {
+    const speakerData = speakers[i];
+    const speakerCard = speakerCards[i].querySelector('img');
+    speakerCard.src = speakerData.img;
+    speakerCard.alt = speakerData.name;
+    const content = speakerCards[i].querySelector('p');
+    content.innerHTML = `
+          <span class="name">${speakerData.name}</span>
+          <br>
+          <span class="proffesionalism">${speakerData.proffesionalism}</span>
+          <br> ${speakerData.description}
+        `;
+  }
+}
 
-<div class="row speakers_cards">
-<div class="col-6">
-    <div class="row">
-        <div class="col-4 speakers">
-            <img src="images/speaker_05.png" alt="">
-        </div>
-        <div class="col-8">
-            <p>
-                <span class="name">Ima Giller</span>
-                <br>
-                <span class="proffesionalism">
-                    professor at Casablanca Law School
-                </span>
-                <br> Focusing on a collaborative approach to Moroccan culture, this October's festival promotes the idea of co-creation based on sharing and openness..</p>
-        </div>
-    </div>
-</div>
-<div class="col-6">
-    <div class="row speakers">
-        <div class="col-4">
-            <img src="images/speaker_06.png" alt="">
-        </div>
-        <div class="col-8">
-            <p>
-                <span class="name">yohai benkler</span>
-                <br>
-                <span class="proffesionalism">professor at Harvard Law School</span>
-                <br> Focusing on a collaborative approach in a networked environment, he created the concept
-                of co-production based on sharing, such as open source software and Wikipedia.</p>
-        </div>
-    </div>
-</div>
-</div>
-`;
-desktopSpeakers.innerHTML += divisionSpeaker;
+Mobile(speakersData);
