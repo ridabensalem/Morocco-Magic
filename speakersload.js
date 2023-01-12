3feb4bc297c65e78c1790bc755a984915785cec6
ridabensalem <ridasbnesalmas@gmail.com>
2023-01-12 11:10:14 +0100

    Load speakers section dinamically

---
+++ speakersload.js
@@ -0,0 +1,115 @@
+const desktopSpeakers = document.querySelector('.container.down_section');
+const divisionSpeaker = ` <div class="row">
+<div class="col-12">
+    <center>
+        <h2>Featured speakers</h2>
+    </center>
+</div>
+</div>
+
+<div class="row speakers_cards">
+<div class="col-6">
+    <div class="row">
+        <div class="col-4 speakers">
+            <img src="images/speaker_01.png" alt="">
+        </div>
+        <div class="col-8">
+            <p>
+                <span class="name">Steven benkler</span>
+                <br>
+                <span class="proffesionalism">
+                    professor at Nessan Law School
+                </span>
+                <br> Practicing  on a collaborative approach in a networked environment, he created the concept
+                of co-production based on sharing, such as open source software and Wikipedia.</p>
+        </div>
+    </div>
+</div>
+<div class="col-6">
+    <div class="row speakers">
+        <div class="col-4">
+            <img src="images/speaker_02.png" alt="">
+        </div>
+        <div class="col-8">
+            <p>
+                <span class="name">Ahmed Magma</span>
+                <br>
+                <span class="proffesionalism">professor at Harvard Law School</span>
+                <br> Focusing on a collaborative approach in a networked environment, he created the concept
+                of co-production based on sharing, such as open source software and Wikipedia.</p>
+        </div>
+    </div>
+</div>
+</div>
+
+<div class="row speakers_cards">
+<div class="col-6">
+    <div class="row">
+        <div class="col-4 speakers">
+            <img src="images/speaker_03.png" alt="">
+        </div>
+        <div class="col-8">
+            <p>
+                <span class="name">Jess Ghith</span>
+                <br>
+                <span class="proffesionalism">
+                    professor at Harvard Law School
+                </span>
+                <br> Focusing on a collaborative approach to Moroccan culture, this October's festival promotes the idea of co-creation based on sharing and openness.</p>
+        </div>
+    </div>
+</div>
+<div class="col-6">
+    <div class="row speakers">
+        <div class="col-4">
+            <img src="images/speaker_04.png" alt="">
+        </div>
+        <div class="col-8">
+            <p>
+                <span class="name">Rebecca benkler</span>
+                <br>
+                <span class="proffesionalism">
+                Tester at Safi Art School
+            </span>
+            <p>Making partnership about new tourisme in north Morocco to the south then testing traverls experiense </p>
+        </div>
+    </div>
+</div>
+</div>
+
+
+<div class="row speakers_cards">
+<div class="col-6">
+    <div class="row">
+        <div class="col-4 speakers">
+            <img src="images/speaker_05.png" alt="">
+        </div>
+        <div class="col-8">
+            <p>
+                <span class="name">Ima Giller</span>
+                <br>
+                <span class="proffesionalism">
+                    professor at Casablanca Law School
+                </span>
+                <br> Focusing on a collaborative approach to Moroccan culture, this October's festival promotes the idea of co-creation based on sharing and openness..</p>
+        </div>
+    </div>
+</div>
+<div class="col-6">
+    <div class="row speakers">
+        <div class="col-4">
+            <img src="images/speaker_06.png" alt="">
+        </div>
+        <div class="col-8">
+            <p>
+                <span class="name">yohai benkler</span>
+                <br>
+                <span class="proffesionalism">professor at Harvard Law School</span>
+                <br> Focusing on a collaborative approach in a networked environment, he created the concept
+                of co-production based on sharing, such as open source software and Wikipedia.</p>
+        </div>
+    </div>
+</div>
+</div>
+`;
+desktopSpeakers.innerHTML += divisionSpeaker;
